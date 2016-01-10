ef539e235ecc1decc8e718f413d1bb6db4df153d
Andrew Lapin <andrew.d.lapin@gmail.com>
2016-01-10 14:02:04 +0300

    Refactor app jsx

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,5 @@ import ReactDOM from 'react-dom'
 import Root from 'components/root'
 
 import 'styles/global.scss'
-import styles from 'app.scss'
 
-ReactDOM.render(
-  <div className={styles['body-wrapper']}>
-    <Root />
-  </div>,
-  document.getElementById('root')
-)
+ReactDOM.render(<Root />, document.getElementById('root'))
