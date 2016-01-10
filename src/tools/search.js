3a7835b4f10106b1b6e5ae186af0485207c4102a
Andrew Lapin <andrew.d.lapin@gmail.com>
2016-01-10 22:37:09 +0300

    Refactor brand search

--- src/tools/search.js
+++ src/tools/search.js
@@ -5,7 +5,7 @@ import _ from 'underscore'
   BrandSearch users nGram based https://en.wikipedia.org/wiki/N-gram search algorythm inspired by
   http://glench.github.io/fuzzyset.js/ but with much more simplier implementation.
 
-  1) We build inverted indexes || ngrams given brand names. Each brand name we split on [3, 2, 1] grams (by default).
+  1) We build inverted indexes || ngramsIndex given brand names. Each brand name we split on [3, 2, 1] grams (by default).
   Example: flxble -> ['flx', 'lxb', 'xbl', 'ble']
 
   2) In order to search for brands we take the n-grams of the query string and perform a reverse index look up for each gram.
@@ -19,36 +19,42 @@ export class BrandSearch {
     this.gramSizeUpper = gramSizeUpper
     this.resultLimit = resultLimit
 
-    this.buildIndex(brands)
+    this.ngramsIndex = this.buildIndex(brands)
   }
 
   buildIndex (brands) {
-    this.ngrams = {}
+    let ngramsIndex = {}
 
-    // initialize ngrams inverted indexes from gramSizeLower to gramSizeUpper
+    // initialize ngramsIndex inverted indexes from gramSizeLower to gramSizeUpper
     for (let i = this.gramSizeLower; i <= this.gramSizeUpper; ++i) {
-      this.ngrams[i] = {}
+      ngramsIndex[i] = {}
     }
 
     // building indexes for each brand
     for (let brand of brands) {
-      const normalizedName = brand.brand_copy[0].brand_name.toLowerCase()
+      this.addBrandToIndex(ngramsIndex, brand)
+    }
 
-      // building indexes for each size of gram
-      for (let gramSize = this.gramSizeLower; gramSize <= this.gramSizeUpper; ++gramSize) {
-        let nGrams = nGram(gramSize)(normalizedName)
+    return ngramsIndex;
+  }
 
-        for (let gram of nGrams) {
-          if (!this.ngrams[gramSize][gram]) {
-            this.ngrams[gramSize][gram] = []
-          }
+  addBrandToIndex (index, brand) {
+    const normalizedName = brand.brand_copy[0].brand_name.toLowerCase()
 
-          if (!_.find(this.ngrams[gramSize][gram], (index) => index.name === normalizedName)) {
-            this.ngrams[gramSize][gram].push({
-              name: normalizedName,
-              brand: brand
-            })
-          }
+    // building indexes for each size of gram
+    for (let gramSize = this.gramSizeLower; gramSize <= this.gramSizeUpper; ++gramSize) {
+      let ngrams = nGram(gramSize)(normalizedName)
+
+      for (let gram of ngrams) {
+        if (!index[gramSize][gram]) {
+          index[gramSize][gram] = []
+        }
+
+        if (!_.find(index[gramSize][gram], (index) => index.name === normalizedName)) {
+          index[gramSize][gram].push({
+            name: normalizedName,
+            brand: brand
+          })
         }
       }
     }
@@ -57,25 +63,9 @@ export class BrandSearch {
   search (query, resultLimit) {
     let normalizedQuery = query.toLowerCase()
 
-    let results = []
+    let results = this._reverseIndexLookup(normalizedQuery)
 
-    // falls down to lower grams once there are no results found
-    for (let gramSize = this.gramSizeUpper; gramSize >= this.gramSizeLower; --gramSize) {
-      let queryGrams = nGram(gramSize)(normalizedQuery)
-
-      for (let gram of queryGrams) {
-        if (this.ngrams[gramSize][gram] && this.ngrams[gramSize][gram].length !== 0) {
-          results = results.concat(this.ngrams[gramSize][gram])
-        }
-      }
-
-      if (results.length > 0) { break }
-    }
-
-    // retrieve brands from each gram of normalizedQuery
-    for (let i = 0; i < results.length; ++i) {
-      results[i].score = this.distance(normalizedQuery, results[i].name)
-    }
+    this._assignScoreToResults(results, normalizedQuery)
 
     results = _.sortBy(results, (index) => -index.score)
     results = _.map(results, (index) => index.brand)
@@ -83,12 +73,37 @@ export class BrandSearch {
     return results.slice(0, resultLimit || this.resultLimit)
   }
 
+  _reverseIndexLookup (normalizedQuery) {
+    let results = []
+
+    // falls down to lower grams once there are no results found
+    for (let gramSize = this.gramSizeUpper; gramSize >= this.gramSizeLower; --gramSize) {
+      let queryGrams = nGram(gramSize)(normalizedQuery)
+
+      for (let gram of queryGrams) {
+        if (this.ngramsIndex[gramSize][gram] && this.ngramsIndex[gramSize][gram].length !== 0) {
+          results = results.concat(this.ngramsIndex[gramSize][gram])
+        }
+      }
+
+      if (results.length > 0) { return results }
+    }
+
+    return results
+  }
+
+  _assignScoreToResults (results, normalizedQuery) {
+    for (let i = 0; i < results.length; ++i) {
+      results[i].score = this._distance(normalizedQuery, results[i].name)
+    }    
+  }
+
   // https://github.com/Glench/fuzzyset.js/blob/master/lib/fuzzyset.js#L41
-  distance (str1, str2) {
+  _distance (str1, str2) {
     if (str1 === null || str2 === null) { return 0 }
     str1 = String(str1); str2 = String(str2)
 
-    var distance = this.levenshtein(str1, str2)
+    var distance = this._levenshtein(str1, str2)
     if (str1.length > str2.length) {
       return 1 - distance / str1.length
     }
@@ -97,7 +112,7 @@ export class BrandSearch {
   }
 
   // https://github.com/Glench/fuzzyset.js/blob/master/lib/fuzzyset.js#L20
-  levenshtein (str1, str2) {
+  _levenshtein (str1, str2) {
     var current = []
     var prev
     var value
