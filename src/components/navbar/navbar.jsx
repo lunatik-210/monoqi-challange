79d73d404478a1f4f0dfa328a2e720dd4f5ea614
Andrew Lapin <andrew.d.lapin@gmail.com>
2016-01-11 22:03:48 +0300

    Add simple search popup to navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,8 +6,20 @@ export default class Navbar extends React.Component {
   render() {
     return (
       <div className={styles['b-navbar']}>
-        <span>Header</span>
+        <div className={styles['b-brand-search-panel']}>
+          <button className={styles['e-search-button']}>BRAND \/</button>
+          <div className={styles['b-search-pop-up']}>
+            <div className={styles['b-search-pop-up-content']}>
+              <input className={styles['e-search-input']} type="text"/>
+              <span>CURRENTLY SELECTED</span>
+              <span>__________________</span>
+              <span>BRANDS MATCHING YOUR SEARCH</span>
+              <span>__________________</span>
+              <button>APPLY SELECTION</button>
+            </div>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
